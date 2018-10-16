2f3b851970178b2dfad7a5c9c87211ecad8e56be
Royaaaaaa <43308173+Royaaaaaa@users.noreply.github.com>
2018-10-16 16:17:19 +1100

    Revert "Merge pull request #3 from GaoYu620/MayaBranch"
    This reverts commit d0ee179f671e3dc1da971546f96911fe62083766, reversing
    changes made to 316291b0ddebcf9455e7e48393ebb71f9ee33d05.
    
    revert merge d0ee179

---
+++ SafeCrossing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SafeCrossing
+//
+//  Created by YU MUSE on 16/10/18.
+//  Copyright © 2018 YU MUSE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
