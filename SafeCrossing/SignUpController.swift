3b5d1ebd66e69f4deba16fffc3b04da59e0f90df
Royaaaaaa <43308173+Royaaaaaa@users.noreply.github.com>
2018-10-16 15:32:02 +1100

    sign up

---
+++ SafeCrossing/SignUpController.swift
@@ -0,0 +1,57 @@
+//
+//  SignUpController.swift
+//  SafeCrossing
+//
+//  Created by 隋晓婷 on 2018/10/16.
+//  Copyright © 2018 YU MUSE. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class SignUpController: UIViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var confirmPasswordTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func signUpButton(_ sender: Any) {
+        if passwordTextField.text != confirmPasswordTextField.text {
+            let alertController = UIAlertController(title: "Password Incorrect", message: "Please re-type password", preferredStyle: .alert)
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            
+            alertController.addAction(defaultAction)
+            self.present(alertController, animated: true, completion: nil)
+        }
+        else{
+            Auth.auth().createUser(withEmail: emailTextField.text!, password: passwordTextField.text!){ (user, error) in
+                if error == nil {
+                    self.performSegue(withIdentifier: "signupToHome", sender: self)
+                }
+                else{
+                    let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                    let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                    
+                    alertController.addAction(defaultAction)
+                    self.present(alertController, animated: true, completion: nil)
+                }
+            }
+        }
+    }
+    /*
+     // MARK: - Navigation
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+
+}
